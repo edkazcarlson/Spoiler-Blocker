c45d19c7aa8ee8959f576f95de0d8f658836463a
Edward Carlson <kazuya@comcast.net>
2020-08-10 14:41:33 -0700

    working state

--- blocker.js
+++ blocker.js
@@ -1,27 +1,50 @@
+async function getLocalStorageValue(key) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(key, function (value) {
+                resolve(value);
+            });
+        }
+        catch (ex) {
+            reject(ex);
+        }
+    });
+}
+
+let importantCharSet = '';
+window.onload = async function(){
+	importantCharSet= await getLocalStorageValue('names');
+	importantCharSet = new Set(importantCharSet.names);
+}
+
 let autoCompleteWatchingReady = false;
 window.onkeyup = function(){
-  autoCompleteWatchingReady = true;
+    autoCompleteWatchingReady = true;
 }
 
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
-    // fired when a mutation occurs
-    if (autoCompleteWatchingReady){
-      $('.sbqs_c:contains("death")').each(function(idx, val){
-        val.innerHTML = val.innerHTML.replace('death', 'fight');
-        if (idx >= 14){
-          return false;
-        }
-        autoCompleteWatchingReady = false;
-      });       
-    }
+	// fired when a mutation occurs
+	if (autoCompleteWatchingReady){
+		$('.sbqs_c:contains("death")').each(function(idx, val){
+			let deathLineSet = new Set(val.innerText.toLowerCase().split(' '));
+			let intersection = new Set([...deathLineSet].filter(x => importantCharSet.has(x)));
+			if (intersection.size > 0){
+				val.innerHTML = val.innerHTML.replace('death', 'fight');
+			}
+			if (idx >= 14){
+				return false;
+			}
+		});   
+		autoCompleteWatchingReady = false;          
+	}
 });
 
 observer.observe(document, {
-  subtree: true,
-  attributes: true,
-  childList: true,
-  // characterData: true
+    subtree: true,
+    attributes: true,
+    childList: true,
+    // characterData: true
 });
--- popup.js
+++ popup.js
@@ -1,38 +1,70 @@
 window.addEventListener("load", init);
 function init(){
-    getCharacterList();
-    document.getElementById("enterButton").addEventListener("click", onEntry)
-}
-function onEntry(){
-    chrome.storage.sync.set({'apiKey': $("#apiKey")[0].value});
-    chrome.storage.sync.set({'username': $("#username")[0].value});
-
+    updateLabels();
+    document.getElementById("enterButton").addEventListener("click", setUserInfo);
 }
 
-function customConsole(input){
-    document.getElementById("console").innerText = input;
+async function updateLabels(){
+    let username = (await getLocalStorageValue('username')).username;
+    if (username === undefined){
+        $('#lastUsername')[0].innerText = 'Enter username above';
+    } else {
+        $('#lastUsername')[0].innerText = 'Current Username: ' + username;
+        $('#lastUpdated')[0].innerText = 'Last Updated: ' + (await getLocalStorageValue('lastUpdated')).lastUpdated
+    }
 }
 
-async function getCharacterList(apiKey, username){
+function setUserInfo(){
+    chrome.storage.local.set({username: $("#username")[0].value});
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); 
+    let yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    chrome.storage.local.set({lastUpdated: today});
+    updateLabels();
+    refresh();
+}
+
+async function getLocalStorageValue(key) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(key, function (value) {
+                resolve(value);
+            })
+        }
+        catch (ex) {
+            reject(ex);
+        }
+    });
+}
+
+async function refresh(){
+    let username  = (await getLocalStorageValue('username')).username;
+    getCharacterList(username);
+}
+async function getCharacterList(username){
     let response = await fetch('http://api.jikan.moe/v3/user/' + username + '/animelist/watching');
     let jsonData = await response.json();
     let showIDs = []
     for (var show in jsonData.anime){
         showIDs.push(jsonData.anime[show].mal_id)
     }
-    let charNames = []
+    let charNames = new Set()
     for (var show of showIDs){
         let showURL = 'https://api.jikan.moe/v3/anime/' + show + '/characters_staff';
         let charResponse = await fetch(showURL);
         let charJson = await charResponse.json();
         for (var char in charJson.characters){
-            char = charJson.characters[char];
-            if (char.role == 'Main'){
-                charNames.push(char.name);
+            char = charJson.characters[char].name;
+            char = char.replace(",", "");
+            char = char.split(' ');
+            for (var c in char){
+                charNames.add(char[c].toLowerCase());
             }
-
         }
     };
-    console.log(charNames)
+    charNames.delete('death');
+    chrome.storage.local.set({names: Array.from(charNames)});
 }
 
